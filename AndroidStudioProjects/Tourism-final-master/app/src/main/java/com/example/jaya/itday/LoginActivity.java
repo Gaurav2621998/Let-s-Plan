0fa5926446635a03e2cf6f9e4aa46d7df94acb3f
gaurav <jaingaurav112@gmail.com>
2018-03-20 15:15:58 +0530

    new

--- AndroidStudioProjects/Tourism-final-master/app/src/main/java/com/example/jaya/itday/LoginActivity.java
+++ AndroidStudioProjects/Tourism-final-master/app/src/main/java/com/example/jaya/itday/LoginActivity.java
@@ -6,7 +6,10 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< 0f3d8e8c08375c7bcedfcd915076a4870f209f5e
 import android.widget.ProgressBar;
+=======
+>>>>>>> new
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,14 +38,22 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+<<<<<<< 0f3d8e8c08375c7bcedfcd915076a4870f209f5e
         setContentView( R.layout.activity_main11 );
+=======
+        setContentView( R.layout.activity_main3 );
+>>>>>>> new
 
 
         // GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
         //.requestIdToken(getString(R.string.default_web_client_id))
         //.requestEmail()
         //.build();
+<<<<<<< 0f3d8e8c08375c7bcedfcd915076a4870f209f5e
         final ProgressBar p=(ProgressBar)findViewById(R.id.progressBar2);
+=======
+
+>>>>>>> new
         googleButton = (Button)findViewById(R.id.google);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -56,8 +67,11 @@ public class LoginActivity extends AppCompatActivity {
         googleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+<<<<<<< 0f3d8e8c08375c7bcedfcd915076a4870f209f5e
                 p.setVisibility(View.VISIBLE);
                 googleButton.setVisibility(View.GONE);
+=======
+>>>>>>> new
                 signIn();
             }
         });
