6500dadd9fb80b216a488ef3197cc5362844312a
gaurav <jaingaurav112@gmail.com>
2018-03-21 01:32:20 +0530

    new n

--- AndroidStudioProjects/Tourism-final-master/app/src/main/java/com/example/jaya/itday/Main8Activity.java
+++ AndroidStudioProjects/Tourism-final-master/app/src/main/java/com/example/jaya/itday/Main8Activity.java
@@ -1,39 +1,13 @@
 package com.example.jaya.itday;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.view.View;
-
-import java.util.ArrayList;
 
 public class Main8Activity extends AppCompatActivity {
-
-    CardView c,d;
-    //String[] arr={"City Palace","Dudh Talai","Jag Mandir","FatahSagar" };
-    ArrayList<String> s=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
-
-        c=(CardView)findViewById(R.id.c1);
-        //d=(CardView)findViewById(R.id.c2);
-        s.add("City Palace");
-        s.add("Dudh Talai");
-        s.add("Jag Mandir");
-        s.add("FatahSagar");
-        c.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(Main8Activity.this,Main3Activity.class);
-                i.putExtra("list",s);
-                startActivity(i);
-
-
-            }
-        });
     }
 }
